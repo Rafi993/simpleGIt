52d12a44ba08fcacef0ce163df6ec3061744ab40
mohamedrafi <fair7993@gmail.com>
2018-03-12 20:46:30 +0530

    @shame removed from commandLine.js

--- commandLine.js
+++ commandLine.js
@@ -5,6 +5,10 @@ const {
   drop
 } = require('ramda')
 
+const {
+  noArgs
+} = require('./util')
+
 /**
  * This function takes the command line params and parses it and returns the object 
  * with type and val if error type is returned as 'error'
@@ -12,51 +16,40 @@ const {
  */
 exports.commandLine = args => {
 
-  let res = {
-    type: '',
-    val: '',
+  const processArgs = argsArr => {
+    const firstArg = argsArr[0] || '';
+
+    const getSubCommand = {
+      'init': argsArr[1].length > 0 ? argsArr[1] + '/' : argsArr[1],
+      'add': argsArr[1],
+      'hash-object': argsArr[1],
+      'commit': argsArr.length >= 3 ?
+        // Check if the third argument is -m else thro error
+        (argsArr[1] === '-m' ? [argsArr[1] || '', argsArr[2]] : 'error')
+        // Check if the argument list is greater than or equal to 3 else throw error
+        :
+        'error'
+    }
+    const subCommand = getSubCommand[firstArg];
+
+    // Check if error else return Sub-command with type
+    if (subCommand === 'error') {
+      return {
+        type: 'error',
+        val: 'Check if your arguments are correct'
+      }
+    } else {
+      return {
+        type: firstArg,
+        val: subCommand
+      }
+    }
+
   }
 
   // Remove node path and file path from args and convert all args to lower case
   const slicedArgs = map(toLower, drop(2, args))
 
   // Check if user has passed any params since first arg is node path, second one is file path
-  if (slicedArgs.length > 0) {
-
-
-    // Assign first user passed args which is sub-command to type
-    res.type = slicedArgs[0]
-
-    // @shame comeup with better way to parse args without or switch
-    // Depending on sub-command pass it value
-    switch (res.type) {
-      case 'init':
-        res.val = slicedArgs[1] || ''
-        if (res.val.length > 0) {
-          res.val += '/'
-        }
-        break;
-      case 'add': res.val = slicedArgs[1] || ''; break;
-      case 'hash-object': res.val = slicedArgs[1] || ''; break;
-      case 'commit':
-        if(slicedArgs.length >= 3) {
-          if(slicedArgs[1] === '-m') {
-            res.val = [ slicedArgs[1] || '', slicedArgs[2]]
-          }
-        } else {
-          res.type = 'error'
-          res.val = 'Not enough arguments'
-        }; break;
-      default:
-        res.val = 'Unknown option';
-        res.type = 'error'
-    }
-
-  } else {
-    // If user didnt pass any sub-command
-    res.type = 'error'
-    res.val = 'Lets try with an argument again'
-  }
-  return res
-
+  return slicedArgs.length > 0 ? processArgs(slicedArgs) : noArgs()
 }
